527d660c695f78c722b1d47f73a622ec49ce96af
Borodkin <borodkinviktor@gmail.com>
2022-11-06 19:38:38 +0300

    Домашняя работа

---
+++ Задача-19/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+
+    Console.WriteLine("Введите пятизначное число");
+    int number = int.Parse(Console.ReadLine() ??"");
+    Console.WriteLine();
+    
+    if(number < 100000 && number > 9999)
+     
+        
+       Console.Write($" Число {number} ");
+    else
+    {
+        Console.WriteLine("Введено не пятизначное число. Попробуйте еще раз))");
+        return;
+    }   
+    string numberAsString = number.ToString();
+    int indexNULL = int.Parse(numberAsString[0].ToString());
+    int indexFIRST = int.Parse(numberAsString[1].ToString());
+    int indexSECOND = int.Parse(numberAsString[2].ToString());
+    int indexTHIRD = int.Parse(numberAsString[3].ToString());
+    int indexFORTH = int.Parse(numberAsString[4].ToString());
+    if (indexNULL ==  indexFORTH && indexFIRST == indexTHIRD)
+        Console.WriteLine("   ЯВЛЯЕТСЯ ПАЛИАНДРОМ");
+    else
+    {
+        Console.WriteLine("   НЕ ЯВЛЯЕТСЯ ПАЛИНДРОМОМ.");
+        return;
+    }
+    
---
+++ Задача-21/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+
+
+Console.WriteLine("Введите Х точки A: ");
+int x1 = int.Parse(Console.ReadLine() ??"");
+Console.WriteLine("Введите Y точки A: ");
+int y1 = int.Parse(Console.ReadLine() ??"");
+Console.WriteLine("Введите Z точки A: ");
+int z1 = int.Parse(Console.ReadLine() ??"");
+Console.WriteLine("Введите Х точки B: ");
+int x2 = int.Parse(Console.ReadLine() ??"");
+Console.WriteLine("Введите Y точки B: ");
+int y2 = int.Parse(Console.ReadLine() ??"");
+Console.WriteLine("Введите Z точки B: ");
+int z2 = int.Parse(Console.ReadLine()??"");
+
+int sqrLenghtX = (x2 - x1) * (x2 - x1);
+int sqrLenghtY = (y2 - y1) * (y2 - y1);
+int sqrLenghtZ = (z2 - z1) * (z2 - z1);
+
+double distance = Math.Sqrt(sqrLenghtX + sqrLenghtY + sqrLenghtZ);
+System.Console.WriteLine("Расстояние равно " + distance);
+
+
+int ReadLine(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ??"");
+}
